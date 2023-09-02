00267a8cec1f774f8be8021659daa4a1de7137cc
adminwebkit <info@adminwebkit.com>
2023-09-02 16:49:38 +0200

    for humanity

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,3 +1,27 @@
+
 document.addEventListener("DOMContentLoaded", function() {
+
+    var menuUsers = document.getElementById('menuUsers');
+    var menuPages = document.getElementById('menuPages');
+
+    var collapseUsers = new bootstrap.Collapse(menuUsers, {
+        toggle: false,
+        show: true,
+        hide: true
+    });
+
+    var collapsePages = new bootstrap.Collapse(menuPages, {
+        toggle: false,
+        show: true,
+        hide: true
+    });
+
+    menuUsers.addEventListener('shown.bs.collapse', event => {
+        collapsePages.hide();
+    });
+
+    menuPages.addEventListener('shown.bs.collapse', event => {
+        collapseUsers.hide();
+    });
   
 });
